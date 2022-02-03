1f244c5d3e521fe29c693b24c7d6424f0e930771
Jerry <jerrycheng314@gmail.com>
2022-02-02 19:47:16 -0600

    Added rotation and translation

--- src/main/java/frc/robot/team3130/SupportingClasses/Coordinates.java
+++ src/main/java/frc/robot/team3130/SupportingClasses/Coordinates.java
@@ -8,15 +8,33 @@ public class Coordinates {
     private final double Y_CONSTANT = 1; // TODO: determine this
     private final double Z_CONSTANT = 1; // TODO: determine this
 
-    Coordinates(double x, double y, double diameter) {
-        convert(x, y, diameter);
+    Coordinates(double x, double y, double diameter, double botX, double botY, double botAngle) {
+        translation(rotation(convert(x, y, diameter), botAngle), botX, botY);
     }
 
-    private void convert(double x, double y, double diameter) {
-        zPos = Z_CONSTANT / diameter;
-        xPos = x * zPos * X_CONSTANT;
-        yPos = y * zPos * Y_CONSTANT;
+    private double[] convert(double x, double y, double diameter) {
+        double[] coords = {
+                x * X_CONSTANT * Z_CONSTANT / diameter,
+                y * Y_CONSTANT * Z_CONSTANT / diameter,
+                Z_CONSTANT / diameter,
+        };
+        return coords;
 
+    }
+
+    private double[] rotation(double[] coords, double botAngle) {
+        double x = coords[0];
+        double y = coords[1];
+        coords[0] = Math.cos(botAngle) * x - Math.sin(botAngle) * y;
+        coords[1] = Math.sin(botAngle) * x + Math.cos(botAngle) * y;
+        return coords;
+
+    }
+
+    private void translation(double[] coords, double botX, double botY) {
+        xPos = coords[0] + botX;
+        yPos = coords[1] + botY;
+        zPos = coords[2];
     }
 
     public double getX() {
