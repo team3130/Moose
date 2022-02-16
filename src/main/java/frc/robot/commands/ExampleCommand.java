c639ea0d5bcf93e3f6de317af3b6ed4526602ea2
Caleb <calebkugel1@gmail.com>
2022-02-16 10:39:55 -0600

    updated example subsystem

--- src/main/java/frc/robot/commands/ExampleCommand.java
+++ src/main/java/frc/robot/commands/ExampleCommand.java
@@ -14,6 +14,7 @@ public class ExampleCommand extends CommandBase {
     public ExampleCommand(ExampleSubsystem subsystem) {
         //mapping to object passed through parameter
         m_subsystem = subsystem;
+        m_requirements.add(subsystem);
     }
 
     /**
