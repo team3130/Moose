1b0092620dacc17c5df3f2387d919f9aca22608c
Grayson-Helgeson <96157698+Grayson-Helgeson@users.noreply.github.com>
2022-02-16 16:08:51 -0600

    Create Climber.java

---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Climber extends SubsystemBase {
+
+// Any variables/fields used in the constructor must appear before the "INSTANCE" variable
+// so that they are initialized before the constructor is called.
+
+    //Create necessary objects
+
+    //Create and define all standard data types needed
+    public Climber() {
+        // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
+        //       in the constructor or in the robot coordination class, such as RobotContainer.
+        var test = "test";
+
+    }
+
+}
+
