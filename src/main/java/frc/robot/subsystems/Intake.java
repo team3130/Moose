7c6a533caa57c89a3eb5ddee9238f144cffa54bb
Robot1235369 <IsaacCA2018@gmail.com>
2022-02-16 23:16:27 +0000

    finished intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -47,7 +47,7 @@ public class Intake extends SubsystemBase {
     }
 
     public void spinMotor(double speed) {
-        if () {
+        if (!m_intakeSolenoid.get() && !m_intakeSolenoid2.get()) {
             m_motor.set(speed);
         } else {
             m_motor.set(0);
