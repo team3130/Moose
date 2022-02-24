b52245fdc9f6d95b04f5ba55899804ca79629da0
jcsong06 <98064341+jcsong06@users.noreply.github.com>
2022-02-23 18:03:23 -0600

    hihi caleb
    literally only juhae and riley coded this pls check

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,19 +1,25 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.magazziCommand;
+import frc.robot.subsystems.Magazine;
 
 /**
  * All objects that are going to be used that are instantiated once should be defined and accessible here
  */
 public class RobotContainer {
     // define subsystems here
+    Magazine m_Magazine = new Magazine();
 
     // reminder that Singletons are deprecated, please do not use them even for subsystems
     // EX: private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
 
     // make getters for subsystems here
-
+    public Magazine getMagazine() {
+        return m_Magazine;
+    }
 
     public RobotContainer() {
         defineButtonBindings();
@@ -23,6 +29,9 @@ public class RobotContainer {
     public static Joystick m_driverGamepad = new Joystick(0);
     public static Joystick m_weaponsGamepad = new Joystick(1);
 
-    private void defineButtonBindings() {}
+    private void defineButtonBindings() {
+        new JoystickButton(m_driverGamepad, RobotMap.LST_BTN_A).whenPressed(new magazziCommand(m_Magazine));
+        // TODO uhhhhhh check everything about Magazine pls caleb idk what i'm doing but also Button A is a placeholder. when in doubt, it's juhae's fault
+    }
 
 }
