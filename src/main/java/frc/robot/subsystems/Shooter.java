790634f8cc1188574ebf63fad6888ce570bcedd6
Caleb <calebkugel1@gmail.com>
2022-02-25 13:30:24 -0600

    deleted duplicate util folder

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
-import static frc.robot.util.Utils.configPIDF;
+import static frc.robot.utils.Utils.configPIDF;
 
 public class Shooter extends SubsystemBase {
     private WPI_TalonFX m_flywheel;
