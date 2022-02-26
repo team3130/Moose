88ea67fa3a74cadb05bebff8e2d959a0c0846c56
Jerry <jerrycheng314@gmail.com>
2022-02-25 22:41:33 -0600

    Changed slider default value to 10

--- src/main/java/frc/robot/subsystems/Chassis.java
+++ src/main/java/frc/robot/subsystems/Chassis.java
@@ -62,22 +62,22 @@ public class Chassis extends SubsystemBase {
     private NetworkTableEntry D = tab.add("Chassis D", 0).getEntry();
 
     private NetworkTableEntry sliderMove = tab
-            .add("Move Speed Sensitivity", 7.5)
+            .add("Move Speed Sensitivity", 10)
             .withWidget(BuiltInWidgets.kNumberSlider)
             .withProperties(Map.of("min", 0, "max", 10))
             .getEntry();
     private NetworkTableEntry sliderTurn = tab
-            .add("Turn Speed Sensitivity", 7.5)
+            .add("Turn Speed Sensitivity", 10)
             .withWidget(BuiltInWidgets.kNumberSlider)
             .withProperties(Map.of("min", 0, "max", 10))
             .getEntry();
 
     public double getMoveSpeedSensitivityFromShuffleboard() {
-        return sliderMove.getDouble(7.5);
+        return sliderMove.getDouble(10);
     }
 
     public double getTurnSpeedSensitivityFromShuffleboard() {
-        return sliderTurn.getDouble(7.5);
+        return sliderTurn.getDouble(10);
     }
 
     // Create and define all standard data types needed
