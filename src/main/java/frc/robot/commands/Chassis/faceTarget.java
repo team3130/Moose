2b9ca32de30cba693cfa16226831ee4e6d510fd8
Robot1235369 <IsaacCA2018@gmail.com>
2022-02-26 01:25:30 +0000

    h

--- src/main/java/frc/robot/commands/Chassis/faceTarget.java
+++ src/main/java/frc/robot/commands/Chassis/faceTarget.java
@@ -1,16 +1,1 @@
-package frc.robot.commands.Chassis;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.RobotMap;
-import frc.robot.sensors.vision.Limelight;
-import frc.robot.subsystems.Chassis;
-public class faceTarget extends CommandBase {
-private final Chassis m_chassis; 
-public faceTarget(Chassis chassis) {
-m_chassis = chassis;
-m_requirements.add(chassis);
-}
-@Override
-public void initialize() {
-m_chassis.configRampRate(RobotMap.kMaxRampRate);
-}
-@Override public void execute() {m_chassis.driveArcade(0, (Limelight.GetInstance().getTx() / 27) * RobotMap.kMaxTurnThrottle, true);}@Override public boolean isFinished() {return Limelight.GetInstance().getTx() < 2;}@Override public void end(boolean interrupted) {m_chassis.configRampRate(0);}}
+package frc.robot.commands.Chassis;import edu.wpi.first.wpilibj2.command.CommandBase;import frc.robot.RobotMap;import frc.robot.sensors.vision.Limelight;import frc.robot.subsystems.Chassis;public class faceTarget extends CommandBase {private final Chassis m_chassis; public faceTarget(Chassis chassis) {m_chassis = chassis;m_requirements.add(chassis);}@Override public void initialize() {m_chassis.configRampRate(RobotMap.kMaxRampRate);}@Override public void execute() {m_chassis.driveArcade(0, (Limelight.GetInstance().getTx() / 27) * RobotMap.kMaxTurnThrottle, true);}@Override public boolean isFinished() {return Limelight.GetInstance().getTx() < 2;}@Override public void end(boolean interrupted) {m_chassis.configRampRate(0);}}
