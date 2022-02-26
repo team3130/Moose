984dc61571847d80fc754c8429720fd5e8de18fd
Dev Patel <dev.awesome9@gmail.com>
2022-02-25 22:45:53 -0600

    Setup AutonDrive and changed Shuffleboard names
    It will be put in a CommandGroup later
    
    LET'S GO NO MERGE CONFLICT
    (Maybe I should start talking to Jerry)

---
+++ src/main/java/frc/robot/commands/Chassis/AutonDrive.java
@@ -0,0 +1,94 @@
+package frc.robot.commands.Chassis;
+
+
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.RobotMap;
+import frc.robot.subsystems.Chassis;
+import frc.robot.subsystems.ExampleSubsystem;
+
+import java.util.Set;
+
+public class AutonDrive extends CommandBase {
+    // defining an instance to be used throughout the command and to be instantiated in the constructor of type parameter
+    private final Chassis m_chassis; //TODO: rename this to the subsystem this is assigned to
+    private Timer timer = new Timer();
+    private double timeLimit = 5;
+     
+
+    public AutonDrive(Chassis subsystem) {
+        //mapping to object passed through parameter
+        m_chassis = subsystem;
+        m_requirements.add(subsystem);
+        
+
+    }
+
+    /**
+     * The initial subroutine of a command.  Called once when the command is initially scheduled.
+     */
+    @Override
+    public void initialize() {
+        timer.start();
+        m_chassis.configRampRate(RobotMap.kMaxRampRate);
+
+    }
+
+    /**
+     * The main body of a command.  Called repeatedly while the command is scheduled.
+     * (That is, it is called repeatedly until {@link #isFinished()}) returns true.)
+     */
+    @Override
+    public void execute() {
+        double moveSpeed = 0.5 * RobotMap.kMaxHighGearDriveSpeed; //Currently running on low gear (week 0), but it go ZOOM anyway
+        m_chassis.driveArcade(moveSpeed, 0, true);
+
+    }
+
+    /**
+     * <p>
+     * Returns whether this command has finished. Once a command finishes -- indicated by
+     * this method returning true -- the scheduler will call its {@link #end(boolean)} method.
+     * </p><p>
+     * Returning false will result in the command never ending automatically. It may still be
+     * cancelled manually or interrupted by another command. Hard coding this command to always
+     * return true will result in the command executing once and finishing immediately. It is
+     * recommended to use * {@link edu.wpi.first.wpilibj2.command.InstantCommand InstantCommand}
+     * for such an operation.
+     * </p>
+     *
+     * @return whether this command has finished.
+     */
+    @Override
+    public boolean isFinished() {
+        // TODO: Make this return true when this Command no longer needs to run execute()
+         
+        if(timer.get() >= timeLimit){
+            timer.stop();
+            return true;
+        }
+
+        else{return false;}
+    }
+
+    /**
+     * The action to take when the command ends. Called when either the command
+     * finishes normally -- that is it is called when {@link #isFinished()} returns
+     * true -- or when  it is interrupted/canceled. This is where you may want to
+     * wrap up loose ends, like shutting off a motor that was being used in the command.
+     *
+     * @param interrupted whether the command was interrupted/canceled
+     */
+    @Override
+    public void end(boolean interrupted) {
+        m_chassis.configRampRate(0); //idk if this really needs to be here
+        m_chassis.driveArcade(0, 0, true);
+      
+
+    }
+}
