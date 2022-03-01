74743fddeb308bfd72fbe8e143c8a1ecf504ab4b
Robot1235369 <IsaacCA2018@gmail.com>
2022-03-01 16:48:18 +0000

    working

--- src/main/java/frc/robot/controls/POVTrigger.java
+++ src/main/java/frc/robot/controls/POVTrigger.java
@@ -5,17 +5,17 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 class POVTrigger extends Trigger {
 
-    private Joystick stick;
-    private int POV;
+private Joystick stick;
+private int POV;
 
-    public POVTrigger(Joystick stick, int POV) {
-        this.stick = stick;
-        this.POV = POV;
-    }
+public POVTrigger(Joystick stick, int POV) {
+this.stick = stick;
+this.POV = POV;
+}
 
-    @Override
-    public boolean get() {
-        return stick.getPOV(0) == POV;
-    }
+@Override
+public boolean get() {
+return stick.getPOV(0) == POV;
+}
 
 }
