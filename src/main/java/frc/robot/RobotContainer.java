0662461266f99405ac1fda54c787504f9a3d79d5
Caleb <calebkugel1@gmail.com>
2022-04-18 12:55:21 -0500

    burh hakjhfkjdsh I forgot a )

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -116,7 +116,7 @@ public class RobotContainer {
             new JoystickButton(m_driverGamepad, RobotMap.LST_BTN_LJOYSTICKPRESS).whenPressed(new Shift(m_chassis));
             new JoystickButton(m_driverGamepad, RobotMap.LST_BTN_MENU).whenPressed(new DeployIntake(m_intake));
             new TriggerButton(m_driverGamepad, RobotMap.LST_AXS_LTRIGGER).whenHeld(new DeployAndSpintake(m_intake, m_magazine, -1));
-            new JoystickButton(m_driverGamepad, RobotMap.LST_BTN_A).whenHeld(new QuegelCommandGroup(m_chassis, m_shooter, m_intake, m_magazine, m_limelight);
+            new JoystickButton(m_driverGamepad, RobotMap.LST_BTN_A).whenHeld(new QuegelCommandGroup(m_chassis, m_shooter, m_intake, m_magazine, m_limelight));
             new JoystickButton(m_driverGamepad, RobotMap.LST_BTN_RBUMPER).whenHeld(new Shoot(m_shooter, m_magazine, m_chassis, m_limelight));
             new JoystickButton(m_driverGamepad, RobotMap.LST_BTN_LBUMPER).whenHeld(new SetFlywheelRPM(m_shooter, m_magazine, m_limelight));
             new JoystickButton(m_driverGamepad, RobotMap.LST_BTN_Y).whenHeld(new DeployAndSpintakeMagazineBack(m_intake, m_magazine, 1));
