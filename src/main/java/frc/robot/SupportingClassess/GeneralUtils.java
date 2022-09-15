2c9034670d350382b306d0ce68b5265541288e6e
Robot1235369 <IsaacCA2018@gmail.com>
2022-09-15 15:50:39 +0000

    done

--- src/main/java/frc/robot/SupportingClassess/GeneralUtils.java
+++ src/main/java/frc/robot/SupportingClassess/GeneralUtils.java
@@ -1,7 +1,1 @@
-package frc.robot.SupportingClassess;
-
-public interface GeneralUtils {
-    public void outputToShuffleboard();
-    public void teleopInit();
-    public void disable();
-}
+package frc.robot.SupportingClassess;public interface GeneralUtils {public void outputToShuffleboard();public void teleopInit();public void disable();}
