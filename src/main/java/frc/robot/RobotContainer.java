6b763d3f1944781b77648ee390a21a03f5cee69a
Robot1235369 <IsaacCA2018@gmail.com>
2022-09-15 18:28:52 +0000

    Revert "added the javadoc stuff for robotcontainer"
    This reverts commit f74fb40387f561298bd853c3117bff616c270ee1.
    
    merging

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -33,7 +33,8 @@ import java.util.List;
 /**
  * All objects that are going to be used that are instantiated once should be defined and accessible here
  */
-public class RobotContainer {
+public class
+RobotContainer {
     private ArrayList<GeneralUtils> m_generalUtils;
     // Supporting classes
     protected WheelSpeedCalculations m_wheelSpeedCalculations = new WheelSpeedCalculations(WheelSpeedCalculations.CurveMechanism.SHOOTER);
